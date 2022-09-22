eab0191b0efcef9af25c14ab5c8a08f27a2cd6ea
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-09-23 01:59:11 +0530

    V1.7 of the API complete with Magic Bricks

--- magicBricks.py
+++ magicBricks.py
@@ -5,6 +5,7 @@ import time
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 app = Flask(__name__)
 
@@ -14,42 +15,63 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 driver = webdriver.Chrome("chromedriver", chrome_options=options)
 driver.get("https://www.magicbricks.com/property-for-rent/residential-real-estate?bedroom=1,2&proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Service-Apartment&Locality=Pratap-Nagar&cityName=Jaipur")
-time.sleep(7)
+driver.maximize_window()
+time.sleep(5)
 cons = driver.find_elements_by_class_name("mb-srp__list")
-count = 0
+count = 1
 flats = []
 for elem in cons:
-    # if count <= 5:
-    print(count)
+    print(f"Reading listing number {count}")
+    print("Reading Title, Description, Rent and Posted by")
     title = elem.find_element_by_class_name('mb-srp__card--title').text
     description = elem.find_element_by_class_name('mb-srp__card--desc--text').text
     rent = elem.find_element_by_class_name('mb-srp__card__price--amount').text.split('₹')[1]
     postedBy = elem.find_element_by_class_name('mb-srp__card__ads--name').text
-    furnishingElem = elem.find_element_by_xpath('.//div[@data-summary="furnishing"]')
+    furnishingElem = elem.find_element_by_css_selector('div[data-summary="furnishing"]')
     furnishing = furnishingElem.find_element_by_class_name('mb-srp__card__summary--value').text
     bhk = int(title.split('BHK')[0])
+    print(f"{title[:10] + '...'} {description[:10] + '...'} {rent} {postedBy} {furnishing} {bhk} \n")
     infoChips = []
     opener = elem.find_element_by_class_name('mb-srp__card__summary__action')
+    print("Reading Chips from all inforamtion buttons")
     opener.click()
+    time.sleep(2)
     infoElem = elem.find_elements_by_class_name('mb-srp__card__summary__list--item')
-    for chip in infoElem:
-        key = chip.find_element_by_class_name('mb-srp__card__summary--label').text
-        value = chip.find_element_by_class_name('mb-srp__card__summary--value').text
+    for i in range(6):
+        key = infoElem[i].find_element_by_class_name('mb-srp__card__summary--label').text
+        value = infoElem[i].find_element_by_class_name('mb-srp__card__summary--value').text
+        print(f"{key} : {value}")
         infoChips.append({key : value})
     postedOn = elem.find_element_by_class_name('mb-srp__card__photo__fig--post').text
-    imgCount = elem.find_element_by_class_name('mb-srp__card__photo__fig--count').text
+    try:
+        imgCountElm = elem.find_element_by_class_name('mb-srp__card__photo__fig--count')
+    except NoSuchElementException:
+        imgCount = 0
+    imgCount = imgCountElm.text
     dateNow = datetime.now()
     updatedOn = dateNow.strftime("%Y-%m-%d %H:%M:%S")
-    imgElem = elem.find_element_by_class_name('mb-srp__card__photo__fig--graphic')
+    try:
+        imgElem = elem.find_element_by_class_name('mb-srp__card__photo__fig--graphic')
+    except NoSuchElementException:
+        print("No images found. Defaulting to no images")
+        imgs = ['https://img.freepik.com/free-vector/houses-concept-illustration_114360-668.jpg']
     imgElem.click()
     time.sleep(8)
-    imgsElem = driver.find_element_by_class_name("masonryGrid").find_elements_by_tag_name("img")
+    try:
+        imgsGrid = driver.find_element_by_class_name("masonryGrid")
+    except NoSuchElementException:
+        print("Image Modal not opened. Defaulting to no images")
+        imgs = ['https://img.freepik.com/free-vector/houses-concept-illustration_114360-668.jpg']
+    print("Opening Image Modal...")
+    imgsElem = imgsGrid.find_elements_by_tag_name("img")
     imgs = []
     _range = len(imgsElem) if len(imgsElem) < 6 else 6
+    print("Found " + str(_range) + " images")
     for i in range(_range):
         imgs.append(imgsElem[i].get_attribute("src"))
     time.sleep(2)
     driver.find_element_by_class_name("leftArrow").click()
+    time.sleep(2)
     flatsInfo = {
         'title': title,
         'description': description,
@@ -67,10 +89,13 @@ for elem in cons:
     }
     count += 1
     driver.execute_script("window.stop();");
+    print("Closing Image Modal...")
     time.sleep(2)
     json_object = json.dumps(flatsInfo, indent=4)
  
     # write to json file in append mode
+    print("Writing to file...")
     with open("data.json", "a") as outfile:
         outfile.write(json_object)
         outfile.write(",")
+    print("Done\n\n\n")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from yaml import load, FullLoader
 import requests
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -24,94 +25,32 @@ def add_headers(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization, data')
     return response
 
-@app.route('/add-data')
-def index():
-    available = magic()
-    for flat in available:
-        title = flat['title']
-        if len(flat['desc']) > 100:
-            description = flat['desc'][:100] + '...'
-        else:
-            description = flat['desc']
-        imgUrl = flat['img']
-        furnishing = flat['furnishing']
-        area = "pratap nagar"
-        bhkFull = flat['title'].split('BHK')
-        bhk = bhkFull[0] + "BHK"
-        imgCount = 1
-        postedBy = flat['postee']
-        rent = flat['rent'].split('₹')[1]
-        source = "Magic Bricks"
-        date = flat['postedOn'].split(":")[-1].strip()
-        dateNow = datetime.now()
-        updatedOn = dateNow.strftime("%Y-%m-%d %H:%M:%S")
-        infoChips = ", ".join(flat['infoChips'][:6])
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO listings (title, description, imgUrl, furnishing, area, bhk, imgCount, postedBy, rent, source, date, updatedOn, infoChips) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", (title, description, imgUrl, furnishing, area, bhk, imgCount, postedBy, rent, source, date, updatedOn, infoChips))
-        mysql.connection.commit()
-        cur.close()
-    # return jsonify(db_data)
-    return "Done"
-
-@app.route('/magic')
-def magic():
-    url = "https://www.magicbricks.com/property-for-rent/residential-real-estate?bedroom=1,2&proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Service-Apartment&Locality=Pratap-Nagar&cityName=Jaipur"
-    response = requests.get(url)
-    flat=[]
-    soup = bs(response.content, 'html.parser') 
-    titleElem = soup.find_all('h2', attrs={'class':'mb-srp__card--title'})
-    descElem = soup.find_all('div', attrs={'class':'mb-srp__card--desc--text'})
-    rentElem = soup.find_all('div', attrs={'class':'mb-srp__card__price--amount'})
-    imgCountElem = soup.find_all('span', attrs={'class':'mb-srp__card__photo__fig--count'})
-    imgElem = soup.find_all('div', attrs={'class':'mb-srp__card__photo__fig'})
-    posteeElem = soup.find_all('div', attrs={'class':'mb-srp__card__ads--name'})
-    furnishingElem = soup.find_all('div', attrs={'data-summary':'furnishing'})
-    infoElem = soup.find_all('div', attrs={'class':'mb-srp__card__summary__list'})
-    for ind, elem in enumerate(titleElem):
-        title = titleElem[ind].text
-        desc = descElem[ind].text
-        if rentElem[ind].text[0] != "₹":
-            rent = "₹" + rentElem[ind].text
-        else:
-            rent = rentElem[ind].text
-        imgCheck = imgElem[ind].text.split('Posted')
-        if imgCheck[0] == 'No Image Available' or imgCheck[0] == 'Request Photos':
-            continue
-        else:
-            postedOn = imgCheck[1]
-        img = imgElem[ind].select('img')[0]['data-src']
-        furnishing = furnishingElem[ind].text.split('Furnishing')[1].strip()
-        posteeInfo= posteeElem[ind].text
-        infoChips = []
-        for child in infoElem[ind].children:
-            labels = child.findAll('div', attrs={'class':'mb-srp__card__summary--label'})
-            values = child.findAll('div', attrs={'class':'mb-srp__card__summary--value'})
-            infoChips.append(f"{labels[0].text} : {values[0].text}")
-        dateNow = datetime.now()
-        updatedOn = dateNow.strftime("%Y-%m-%d %H:%M:%S")
-        flat.append({
-            'title': title,
-            'desc': desc,
-            'rent': rent,
-            'img': img,
-            'furnishing': furnishing,
-            'postee':posteeInfo,
-            'infoChips': infoChips,
-            'postedOn': postedOn,
-            'updatedOn': updatedOn
-        })
-    return jsonify(flat)
-
-@app.route('/update-entries')
-def update():
+@app.route('/add-data/<provider>')
+def index(provider):
+    f = open(f'{provider}.json')
+    data = json.load(f)
     cur = mysql.connection.cursor()
-    cur.execute("SELECT updatedOn FROM listings")
-    db_data = cur.fetchone()
+    for listing in data:
+        title = listing['title']
+        description = listing['description']
+        rent = listing['rent']
+        imgCount = listing['imgCount']
+        imgs = listing['imgs']
+        furnishing = listing['furnishing']
+        postedBy = listing['postedBy']
+        infoChips = listing['infoChips']
+        date = listing['date']
+        area = listing['area']
+        updatedOn = listing['updatedOn']
+        bhk = listing['bhk']
+        source = listing['source']
+
+        cur.execute("INSERT INTO listings (title, description, rent, imgCount, urlList, furnishing, postedBy, infoChips, date, area, updatedOn, bhk, source) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (title, description, rent, imgCount, imgs, furnishing, postedBy, infoChips, date, area, updatedOn, bhk, source))
+        cur.connection.commit()
     cur.close()
-    return jsonify(db_data)
+    return "Added to database"
 
-
-@app.route('/api/v1/available')
+@app.route('/api/v1.7/available')
 def db():
     cur = mysql.connection.cursor()
     q = cur.execute("SELECT * FROM listings;")
@@ -119,21 +58,22 @@ def db():
         allApartments = []
         listings = cur.fetchall()
         for _list in listings:
-            infoChipsList = _list[13].split(', ')
+            infoChipsList = _list[12].split(', ')
+            imgUrlsList = _list[2].split(',')
             allApartments.append({
                 "id": _list[0],
                 "title": _list[1],
-                "description": _list[2],
-                "imgUrl": _list[3],
-                "furnishing": _list[4],
-                "area": _list[5],
-                "bhk": _list[6],
-                "imgCount": _list[7],
-                "postedBy": _list[8],
-                "rent": _list[9],
-                "source": _list[10],
-                "date": _list[11],
-                "updatedOn": _list[12],
+                "description": _list[13],
+                "imgUrl": imgUrlsList,
+                "furnishing": _list[3],
+                "area": _list[4],
+                "bhk": _list[5],
+                "imgCount": _list[6],
+                "postedBy": _list[7],
+                "rent": _list[8],
+                "source": _list[9],
+                "date": _list[10],
+                "updatedOn": _list[11],
                 "infoChips": infoChipsList
             })
     return jsonify(allApartments)
