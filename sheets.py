72cf2035898dde54a048ed64649263d1aee2da8d
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-10-01 16:08:04 +0530

    Sheets API complete for Users Metrics

--- sheets.py
+++ sheets.py
@@ -4,9 +4,6 @@ from flask import Flask, request, session, jsonify
 app = Flask(__name__)
 sa = gspread.service_account(filename='localeyes-95d0d-7dd35becf2f5.json')
 sh = sa.open("Flat Hunter Beta Testing Database")
-userSheet = sh.worksheet("user")
-flatSheet = sh.worksheet("flat")
-allData = userSheet.get_all_values()
 
 @app.after_request
 def add_headers(response):
@@ -18,30 +15,32 @@ def add_headers(response):
 def index():
     return jsonify({'message': 'reponse'})
 
-@app.route('/update-user', methods=['POST'])
-def update_user():
+@app.route('/<sheet>/update', methods=['POST'])
+def update_user(sheet):
+    workSheet = sh.worksheet(sheet)
     data = request.get_json()
     userId = data['userId']
     updateTab = data['updateTab']
     updateValue = data['updateValue']
-    found = False
-    for row in allData:
-        if userId in row:
-            found = True
-            workingCell = userSheet.find(userId)
-            cellToUpdate = f'{updateTab}{workingCell.row}'
-            if updateValue == 'increment':
-                print("Increment Detected")
-                updateValue = int(userSheet.acell(cellToUpdate).value) + 1
-                print("New Value will be: ", updateValue)
-            print("Updating Cell ", cellToUpdate, " with value ", updateValue)
-            break
-    if found:
-        userSheet.update_acell(cellToUpdate, updateValue)
-        return jsonify({'message': 'success'})
-    userSheet.update_acell(cellToUpdate, updateValue)
+    workingCell = workSheet.find(userId)
+    cellToUpdate = f'{updateTab}{workingCell.row}'
+    if updateValue == 'increment':
+        print("Increment Detected")
+        updateValue = int(workSheet.acell(cellToUpdate).value) + 1
+        print("New Value will be: ", updateValue)
+    print("Updating Cell ", cellToUpdate, " with value ", updateValue)
+    workSheet.update_acell(cellToUpdate, updateValue)
     print("Done...")
-    return jsonify({'message': data})
+    return jsonify({
+        'request': 'success',
+        'message': 'Your Response has been recorded',
+        'insights': {
+            'cellToUpdate': cellToUpdate,
+            'updatedValue': updateValue,
+            'sheet': sheet
+            },
+        }
+    )
 
 
 if __name__ == '__main__':
