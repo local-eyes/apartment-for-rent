48108ad03ea74d53322b203880f3e9aa2c256e36
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-10-09 12:35:19 +0530

    changes for multiple append values

--- sheets.py
+++ sheets.py
@@ -1,4 +1,5 @@
 import gspread
+from math import floor
 from flask import Flask, request, session, jsonify
 
 app = Flask(__name__)
@@ -21,12 +22,32 @@ def update_user(sheet):
     data = request.get_json()
     userId = data['userId']
     updateTab = data['updateTab']
-    updateValue = data['updateValue']
-    workingCell = workSheet.find(userId)
+    updateValueOps = data['updateValue']
+    operation, updateValue = updateValueOps.split(",[")
+    if sheet == 'user':
+        workingCell = workSheet.find(userId)
+        print(workingCell)
+    else:
+        first_column = workSheet.range("A1:A{}".format(workSheet.row_count))
+        found_cell_list = [found for found in first_column if found.value == userId]
+        workingCell = found_cell_list[0]
+        print(workingCell)
+
     cellToUpdate = f'{updateTab}{workingCell.row}'
-    if updateValue == 'increment':
+    if operation == 'increment':
         print("Increment Detected")
-        updateValue = int(workSheet.acell(cellToUpdate).value) + 1
+        updateValue = int(workSheet.acell(cellToUpdate).value) + floor(float(updateValue))
+        print("New Value will be: ", updateValue)
+    if operation == 'append':
+        print("Append Detected")
+        updateValue = updateValue[:-1]
+        if workSheet.acell(cellToUpdate).value == '' or workSheet.acell(cellToUpdate).value == None:
+            updateValue = updateValue
+        else:
+            updateValue = str(workSheet.acell(cellToUpdate).value) + ", " + updateValue
+        print("New Value will be: ", updateValue)
+    if operation == 'overwrite':
+        print("Overwrite Detected")
         print("New Value will be: ", updateValue)
     print("Updating Cell ", cellToUpdate, " with value ", updateValue)
     workSheet.update_acell(cellToUpdate, updateValue)
