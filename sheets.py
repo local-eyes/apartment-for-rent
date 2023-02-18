c043d1cd02d0d39c4c2fc47ce86ec84ba3fb7d13
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2023-02-18 21:22:21 +0530

    code cleaning and user tracking automation

--- sheets.py
+++ sheets.py
@@ -5,6 +5,7 @@ from flask import Flask, request, session, jsonify
 app = Flask(__name__)
 sa = gspread.service_account(filename='localeyes-95d0d-7dd35becf2f5.json')
 sh = sa.open("Flat Hunter Beta Testing Database")
+progressSheet = sa.open("LocalEyes Growth Stats")
 
 @app.after_request
 def add_headers(response):
@@ -23,7 +24,10 @@ def update_user(sheet):
     userId = data['userId']
     updateTab = data['updateTab']
     updateValueOps = data['updateValue']
-    operation, updateValue = updateValueOps.split(",[")
+    operation, updateValue = updateValueOps.split(",")[:1], updateValueOps.split(",")[1:]
+    updateValue = ", ".join(updateValue)
+    updateValue = updateValue.replace("[", "").replace("]", "")
+    print(operation[0], updateValue)
     if sheet == 'user':
         workingCell = workSheet.find(userId)
         print(workingCell)
@@ -31,22 +35,20 @@ def update_user(sheet):
         first_column = workSheet.range("A1:A{}".format(workSheet.row_count))
         found_cell_list = [found for found in first_column if found.value == userId]
         workingCell = found_cell_list[0]
-        print(workingCell)
 
     cellToUpdate = f'{updateTab}{workingCell.row}'
-    if operation == 'increment':
+    if operation[0] == 'increment':
         print("Increment Detected")
         updateValue = int(workSheet.acell(cellToUpdate).value) + floor(float(updateValue))
         print("New Value will be: ", updateValue)
-    if operation == 'append':
+    if operation[0] == 'append':
         print("Append Detected")
-        updateValue = updateValue[:-1]
         if workSheet.acell(cellToUpdate).value == '' or workSheet.acell(cellToUpdate).value == None:
             updateValue = updateValue
         else:
             updateValue = str(workSheet.acell(cellToUpdate).value) + ", " + updateValue
         print("New Value will be: ", updateValue)
-    if operation == 'overwrite':
+    if operation[0] == 'overwrite':
         print("Overwrite Detected")
         print("New Value will be: ", updateValue)
     print("Updating Cell ", cellToUpdate, " with value ", updateValue)
@@ -62,6 +64,41 @@ def update_user(sheet):
             },
         }
     )
+
+@app.route('/update-users', methods=['POST'])
+def update_user_tracking():
+    # append to the end of the sheet
+    workSheet = progressSheet.worksheet("Users Report")
+    first_column = workSheet.range("C1:C{}".format(workSheet.row_count))
+    found_cell_list = [found for found in first_column if found.value == '']
+    workingRow = found_cell_list[0]
+    rowToUpdate = workingRow.row
+    data = request.get_json()
+    for key, value in data.items():
+        cellToUpdate = f'{key}{rowToUpdate}'
+        workSheet.update_acell(cellToUpdate, value)
+    return {
+        "message": "Sheet Updated Successfully",
+        "status": "OK"
+    }
+
+
+@app.route('/update-content', methods=['POST'])
+def update_content():
+    # append to the end of the sheet
+    workSheet = progressSheet.worksheet("Content Report")
+    first_column = workSheet.range("B1:B{}".format(workSheet.row_count))
+    found_cell_list = [found for found in first_column if found.value == '']
+    workingRow = found_cell_list[0]
+    rowToUpdate = workingRow.row
+    data = request.get_json()
+    for key, value in data.items():
+        cellToUpdate = f'{key}{rowToUpdate}'
+        workSheet.update_acell(cellToUpdate, value)
+    return {
+        "message": "Sheet Updated Successfully",
+        "status": "OK"
+    }
 
 
 if __name__ == '__main__':
